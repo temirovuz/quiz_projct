d75cad650bfa2672e2c0511cf1144c85dc5f8292
temirovuz <temirov-muhammad@mail.ru>
2024-01-25 19:12:55 +0500

    add

--- QuizProject/settings/local.py
+++ QuizProject/settings/local.py
@@ -7,17 +7,17 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '../db.sqlite3',  # This is where you put the name of the db file.
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': '../db.sqlite3',  # This is where you put the name of the db file.
         # If one doesn't exist, it will be created at migration time.
-    }
-    # "default": {
-    #     "ENGINE": config("SQL_ENGINE"),
-    #     "NAME": config("SQL_DATABASE"),
-    #     "USER": config("SQL_USER"),
-    #     "PASSWORD": config("SQL_PASSWORD"),
-    #     "HOST": config("SQL_HOST"),
-    #     "PORT": config("SQL_PORT"),
     # }
+    "default": {
+        "ENGINE": config("SQL_ENGINE"),
+        "NAME": config("SQL_DATABASE"),
+        "USER": config("SQL_USER"),
+        "PASSWORD": config("SQL_PASSWORD"),
+        "HOST": config("SQL_HOST"),
+        "PORT": config("SQL_PORT"),
+    }
 }
