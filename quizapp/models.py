82343409eb21e706923209306ac6069698c6d3c0
temirovuz <temirov-muhammad@mail.ru>
2024-01-25 16:04:07 +0500

    add test

--- quizapp/models.py
+++ quizapp/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-
 User = get_user_model()
+
 
 class QuizType(models.Model):
     name = models.CharField(max_length=50)
@@ -10,6 +10,7 @@ class QuizType(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Question(models.Model):
     quiz = models.ForeignKey(QuizType, on_delete=models.CASCADE)
@@ -29,6 +30,7 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Result(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='results')
--- quizapp/tests.py
+++ quizapp/tests.py
@@ -5,21 +5,58 @@ from quizapp.api.v1.serializers import QuizTypeSerializer
 from quizapp.models import QuizType
 
 
-class TestQuizType(TestCase):
+class TestQuizTypeSerializer(TestCase):
     def setUp(self):
-        self.quiz_type = QuizType.objects.creat(name='Test quiz type')
+        self.quiz_type = QuizType.objects.create(name='Test quiz type')
 
     def test_data(self):
-        serializers = QuizTypeSerializer(self.quiz_type).data
+        serializer = QuizTypeSerializer(self.quiz_type).data
 
-        assert serializers['id'] == self.quiz_type.id
+        assert serializer['id'] == self.quiz_type.id
 
 
-class HelloWorld(TestCase):
-    def setUp(self):
-        self.url = reverse('hello world')
+class TestHelloWord(TestCase):
+    def setUp(self) -> None:
+        self.url = reverse('hello_world')
 
     def test_gets(self):
-        respomse = self.client.get(self.url)
-        assert respomse.status_code == 200
-        assert respomse.json()['message'] == 'hello world'
+        response = self.client.get(self.url)
+        assert response.status_code == 200
+        assert response.json()['message'] == 'hello world'
+
+
+class TestQuizTypeView(TestCase):
+    def setUp(self):
+        self.quiz_type1 = QuizType.objects.create(name='Test 1')
+        self.quiz_type2 = QuizType.objects.create(name='Test 2')
+
+        self.url = reverse('quiz_types')
+
+    def test_get(self):
+        quiz_types = QuizType.objects.all()
+        serializers = QuizTypeSerializer(quiz_types, many=True)
+
+        response = self.client.get(self.url)
+
+        assert response.status_code == 200
+        assert len(response.json()) == 2
+
+    def test_post_is_vaild(self):
+        pyload = {
+            'name': 'Test',
+            'image': ""
+        }
+
+        response = self.client.post(self.url, data=pyload)
+
+        assert response.status_code == 201
+
+    def test_post_is_invaild(self):
+        pyload = {
+            'names': 'Test',
+            'image': ""
+        }
+
+        response = self.client.post(self.url, data=pyload)
+
+        assert response.status_code == 400
